2e589fcc72ea9301d4e5dbc80b49258ba558c917
Ilan Gavriel <igavriel@gmail.com>
2017-04-03 23:58:47 +0300

    first version

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # ReactExpress
 React Express Demo
+
+based on pluralsight course
+[Building a Full-Stack App with React and Express](https://app.pluralsight.com/player?course=react-express-full-stack-app-build&author=daniel-stern&name=react-express-full-stack-app-build-m1&clip=7&mode=live)
+
+Step by Step
+------------
+create a default package.json
+```javascript
+npm init 
+```
+
+using npm
+```javascript
+npm install -g gulp
+npm install -g bower
+
+npm install --save express
+```
+
---
+++ server/main.js
@@ -0,0 +1,33 @@
+var express = require('express');
+var path = require('path');
+var open = require('open');
+
+var port = 3000;
+var app = new express();
+
+   //res.render(path.join(__dirname, '../app/index.ejs'),{});
+//      res.sendFile(path.join(__dirname, '../app/index.html'));
+//   res.sendFile('../app/index.html');
+
+// which route to process 
+//app.get('/', function(req,res) {
+  //  res.render(path.join(__dirname, '../app/index.ejs'),{});
+    //res.sendFile(path.join(__dirname, '../app/index.html'));
+//} )
+
+app.get('/', function(req,res) {
+    res.render(path.join(__dirname, '../app/index.ejs'),{});
+} )
+
+// listen to the port above
+app.listen(port, function(err){
+    if(err) {
+        // in case of error log it to the console
+        console.log(err);
+    } else {
+        console.log("open local host on port " + port );
+        console.log("local dir is:" + __dirname);
+        open('http://localhost:'+ port);
+    }
+} )
+
